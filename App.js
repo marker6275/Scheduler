8be86e133eb6418b9088efa0006d3603343a332e
Mark Li <mkli627554@gmail.com>
2022-07-12 20:43:10 -0500

    still works

--- App.js
+++ App.js
@@ -1,24 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import CourseList from './components/Courselist';
 
-export default function App() {
+const schedule = {
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
+};
+
+const Banner = ({title}) => (
+  <Text style={styles.banner}>{title}</Text>
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Welcome to my app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Banner tile={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00f',  
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
-  textStyle: {
-    color: '#fff',
+
+  banner: {
+    color: '#888',
     fontSize: 32,
-  }
+  },
 });
+
+export default App;
